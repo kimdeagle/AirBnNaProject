9f91750d88b646c863a95fd4ba0fb92ccf142987
abcdana <abcdanakim@gmail.com>
2021-03-12 16:55:02 +0900

    Update setting

---
+++ src/main/java/com/test/bnna/member/board/review/ReviewController.java
@@ -0,0 +1,32 @@
+package com.test.bnna.member.board.review;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+@Controller
+public class ReviewController {
+	
+	@Autowired
+	private IReviewDAO dao;
+	
+	@RequestMapping(value="/member/board/review/list.action", method={RequestMethod.GET})
+	public String list(HttpServletRequest req, HttpServletResponse resp) {
+		
+		List<ReviewDTO> list=dao.list();
+		
+		req.setAttribute("list", list);
+		
+		return "member.board.review.list";
+	}
+	
+}
---
+++ src/main/java/com/test/bnna/member/board/review/ReviewDAO.java
@@ -0,0 +1,21 @@
+package com.test.bnna.member.board.review;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReviewDAO implements IReviewDAO {
+	
+	@Autowired
+	private SqlSessionTemplate template;
+
+	@Override
+	public List<ReviewDTO> list() {
+		
+		return template.selectList("review.list");
+	}
+
+}
