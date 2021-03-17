5074f251c1497a9bdc4d98a984a11543c8c0bffd
arajo-hub <joara9566@naver.com>
2021-03-17 12:33:38 +0900

    회원 리뷰 게시판 조회/추가/수정/삭제 기능 구현
    1. 리뷰 게시판 조회/추가/수정/삭제 기능 구현
    2. 리뷰 게시글 추가시 첨부파일 다중업로드할 수 있도록 구현
    3. 리뷰 게시판 수정에서 업로드한 첨부파일 삭제 기능 ajax로 구현

---
+++ src/main/java/com/test/bnna/member/board/review/BookForMemberDTO.java
@@ -0,0 +1,10 @@
+package com.test.bnna.member.board.review;
+
+import lombok.Data;
+
+@Data
+public class BookForMemberDTO {
+	
+	private String seq; // 예약번호
+
+}
--- src/main/java/com/test/bnna/member/board/review/ReviewDAO.java
+++ src/main/java/com/test/bnna/member/board/review/ReviewDAO.java
@@ -1,5 +1,7 @@
 package com.test.bnna.member.board.review;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -42,10 +44,100 @@ public class ReviewDAO implements IReviewDAO {
 		
 	}
 
+	/**
+	 * 추천수를 올리는 메서드입니다.
+	 */
 	@Override
 	public void recommendCountUp(String seq) {
 		template.update("review.recommendcntup", seq);
 		
+	}
+
+	/**
+	 * 회원이 작성한 리뷰목록을 가져오는 메서드입니다.
+	 */
+	@Override
+	public List<ReviewForMemberDTO> listForMember(String seqMember) {
+		
+		return template.selectList("review.listForMember", seqMember);
+	}
+
+	/**
+	 * 리뷰번호로 리뷰정보를 가져오는 메서드입니다.
+	 */
+	@Override
+	public ReviewForMemberDTO info(String seq) {
+		
+		return template.selectOne("review.info", seq);
+	}
+
+	/**
+	 * 리뷰번호로 리뷰글을 삭제하는 메서드입니다.
+	 */
+	@Override
+	public int del(String path, String seq) {
+		
+		int result=delFiles(path, seq);
+		
+		if (result>0) {
+			
+			template.delete("review.delPic", seq);
+			return template.delete("review.del", seq);
+		} else {
+			System.out.println("리뷰글의 원본이미지파일을 지우지 못했습니다.");
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * 리뷰번호로 리뷰글에 딸린 이미지들을 서버에서 삭제하는 메서드입니다.
+  	 * @param path 파일들이 존재하는 경로입니다.
+	 * @param seq 리뷰번호입니다.
+	 * @return 성공하면 0이 아닌 값, 실패하면 0을 반환합니다.
+	 */
+	public int delFiles(String path, String seq) {
+		
+		int deleteCnt=0;
+		
+		// 파일명들을 가져온다.
+		List<ReviewPicDTO> list=template.selectList("review.plist", seq);
+		
+		// 삭제한다.
+		for(ReviewPicDTO dto:list) {
+			File file=new File(path+dto.getImage());
+			file.delete();
+			deleteCnt++;
+		}
+		
+		return deleteCnt;
+	}
+
+	@Override
+	public int add(AddReviewDTO dto) {
+		
+		return template.insert("review.add", dto);
+	}
+
+	@Override
+	public int addReviewPic(ArrayList<ReviewPicDTO> plist) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/**
+	 * add()를 통해 추가된 리뷰번호를 가져오는 메서드입니다.
+	 */
+	@Override
+	public int getCurrentReviewSeq() {
+		
+		return template.selectOne("review.getCurrentReviewSeq");
+	}
+
+	@Override
+	public int edit(EditReviewDTO dto) {
+		
+		return template.update("review.edit", dto);
 	}
 
 }
